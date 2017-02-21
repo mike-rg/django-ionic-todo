6705f5ab8c74028ab6a8a96a9276ac5e3da3b881
Miguel Maldonado <miguel.alberto.maldonado@gmail.com>
2017-02-21 18:19:04 -0300

    Se hacen pequeños cambios para probar de forma local

--- django-ionic-todo/ionic_frontend/www/js/controllers.js
+++ django-ionic-todo/ionic_frontend/www/js/controllers.js
@@ -1,49 +1,68 @@
 angular.module('app.controllers', ['ionic'])
 
 // $routeParams,
-.controller('actividadCtrl', ['$resource', '$scope', '$state', '$stateParams', '$ionicPopup', '$ionicListDelegate', 'Actividad',
-function ($resource, $scope, $state, $stateParams, $ionicPopup, $ionicListDelegate, Actividad) {
-    $scope.header = 'Activity list';
+.controller('actividadCtrl', [ '$scope', '$ionicPopup', '$ionicListDelegate', 'Actividad',
+function ($scope, $ionicPopup, $ionicListDelegate, Actividad) {
+
+  $scope.header = 'Activity list';
+  $scope.actividades = Actividad.query();
+  // Cargamos por primera vez la lista de actividades
+
+  $scope.crear_actividad = function() {
+    $ionicPopup.prompt({
+      title: "New task",
+      template: "Enter task",
+      inputPlaceholder: "What do you need to do?",
+      okText: "Create task"
+    }).then(function(res){ // promesa
+      if(res) {
+        actividad = new Actividad({
+          titulo: res,
+          descripcion: res
+        });
+        // $save: metodo de instancia(resource.$save)
+        actividad.$save();
+        actualizar()
+      }
+    })  // Fin promesa
+  }; // Fin de crear_actividad
+
+  $scope.editar_actividad = function(actividad) {
+    $scope.data = { response: actividad.titulo };
+    $ionicPopup.prompt({
+      title: "Edit task",
+      scope: $scope
+    }).then(function(res){ // promesa
+      if(res != undefined) {
+        // 'res' contiene todos las actividades en un array.
+        // 'actividad' es una instancia del modelo Actividad.
+        Actividad.get({ id: actividad.id }, function(actividadToUpdate){
+          actividadToUpdate.titulo = $scope.data.response;
+          actividadToUpdate.descripcion = $scope.data.response;
+          Actividad.update({ id:actividad.id }, actividadToUpdate );
+        });
+        actualizar()
+      };
+
+      $ionicListDelegate.closeOptionButtons();
+    })  // Fin promesa
+  }; // Fin de editar_actividad
+
+  // Corregir. Actualizar lista de actividades cuando se elimina una actividad
+  $scope.eliminar_actividad = function(actividad) {
+    // delete: metodo del modelo Actividad
+    Actividad.delete({ id: actividad.id })
+    actualizar()
+  }; // Fin de eliminar_actividad
+
+  $scope.actualizar_lista = function(){
     $scope.actividades = Actividad.query();
+  }
 
-    $scope.crear_actividad = function() {
-      $ionicPopup.prompt({
-        title: "New task",
-        template: "Enter task",
-        inputPlaceholder: "What do you need to do?",
-        okText: "Create task"
-      }).then(function(res){ // promesa
-        if(res) {
-          actividad = new Actividad({
-            titulo: res,
-            descripcion: res
-          });
-          actividad.$save()
-        }
-      })
-    };
+  function actualizar() {
+    setTimeout(function(){
+      window.location.reload(1);
+    }, 1000);
+  }
 
-    // Corregir. Capturar correctamente el id de la actividad y hacer update
-    $scope.editar_actividad = function(actividad) {
-      $scope.data = { response: actividad.titulo };
-      $ionicPopup.prompt({
-        title: "Edit task",
-        scope: $scope
-      }).then(function(res){ // promesa
-        if(res != undefined) {
-          // 'res' contiene todos las actividades en un array.
-          // 'actividad' es una instancia del modelo actividad.
-          Actividad.query({id: actividad.id}, function(res){
-            actividad.titulo = $scope.data.response;
-            console.log(actividad)
-          });
-        }; // Fin promesa
-        $ionicListDelegate.closeOptionButtons()
-      })
-    }; // Fin de editar actividad
-
-    // Corregir. Actualizar lista de actividades cuando se elimina una actividad
-    $scope.eliminar_actividad = function(actividad) {
-      Actividad.delete({id: actividad.id})
-    }; // Fin de e acliminar actividad
 }])
